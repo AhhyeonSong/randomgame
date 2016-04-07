56ab73836502a46701a6c6d4789fb6d971350438
Ahhyeon Song <thddkgus36@gmail.com>
2016-04-07 20:55:51 +0900

    #5 add random image and logic

--- app/src/main/java/kr/hs/emirim/thddkgus96/zicobar/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/thddkgus96/zicobar/MainActivity.java
@@ -1,14 +1,16 @@
 package kr.hs.emirim.thddkgus96.zicobar;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+public class MainActivity extends Activity implements View.OnClickListener{
+
     EditText mName;
     Button mButton;
 
@@ -18,6 +20,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         mName = (EditText) findViewById(R.id.name);
         mButton = (Button) findViewById(R.id.btn_show_me_the_chicekn);
